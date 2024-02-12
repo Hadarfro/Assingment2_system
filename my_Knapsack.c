53395f2dd7d48d0e4e25d6f71c3ac8bbb2c7a327
Hadarfro <122720878+Hadarfro@users.noreply.github.com>
2024-02-12 22:04:32 +0200

    finale fix

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -51,7 +51,7 @@ int main(){
     int maxProfit = 0;
     insertToList();
      maxProfit = knapSack (weights, values, selected_bool);
-    printf("Maximum profit : %d \n", maxProfit);
+    printf("Maximum profit: %d \n", maxProfit);
     printf("Selected items:");
     for (int i = 0; i < size; i++)
     {
