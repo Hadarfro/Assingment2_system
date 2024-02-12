d756f8342eeb857b0252d6bef78cce7d7c3203c8
Hadarfro <122720878+Hadarfro@users.noreply.github.com>
2024-02-12 21:46:35 +0200

    fixing my_Knapsack

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -19,31 +19,30 @@ void  insertToList(){
 }
 
 int knapSack (int weights[], int values[] , int selected_bool[]){
-  int table[size+1][size+1];
+  int table[size+1][whight_bag+1];
   int w = whight_bag;
-  int wItem , vItem ;
   int sumValue = 0;
-  for (size_t i = 1; i < size+1; i++) {
-    wItem = weights[i-1];
-    vItem = values[i-1];
-    for (size_t j = 1; j < whight_bag+1; j++){
-        if(wItem>j){
+  for (int i = 0; i <= size; i++) {
+    for (int j = 0; j <= whight_bag; j++){
+        if(i==0||j==0){
+            table[i][j] = 0;
+        }
+        else if(weights[i-1]>j){
             table[i][j] = table[i-1][j];
         }
         else{
-            table[i][j] = (table[i-1][j] > (table[i-1][j-wItem] + vItem)) ? table[i-1][j] : (table[i-1][j-wItem] + vItem);
+            table[i][j] = (table[i-1][j] > (table[i-1][j-weights[i-1]] + values[i-1])) ? table[i-1][j] : (table[i-1][j-weights[i-1]] + values[i-1]);
         }
       }
-    for (int j = size - 1; j >= 0; j--){
+  }
+    for (int j = size; j > 0; j--){
         if(table[j][w]!=table[j-1][w]){
-            wItem = weights[j-1];
-            vItem = values[j-1];
             sumValue += values[j-1];
-            w -= wItem;
-            selected_bool[j] = 1;
+            w -= weights[j-1];
+            selected_bool[j-1] = 1;
         }
     }   
-  }
+  
   return sumValue;
 }
 
